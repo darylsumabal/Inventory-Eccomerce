395b26860987fd41ebd36f8594afc699fda9b0bc
Legendaryl <darylsumabal123@gmail.com>
2025-08-13 13:25:47 +0800

    ok

--- src/app/inventory/page.tsx
+++ src/app/inventory/page.tsx
@@ -3,6 +3,7 @@ import TableInventory from "./table-inventory";
 
 export default async function Page() {
   const products = await getAllProducts();
+  console.log(process.env.DATABASE_URL);
 
   return (
     <div className="w-full px-4 lg:px-6">
